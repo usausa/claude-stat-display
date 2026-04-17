2021648c12b19be68e9a548728086b8db3530de4
usausa <machi.pon@gmail.com>
2026-04-17 09:51:23 +0900

    Update

---
+++ ClaudeStatDisplay/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Hosting.WindowsServices;
+
+using Serilog;
+
+//--------------------------------------------------------------------------------
+// Configure builder
+//--------------------------------------------------------------------------------
+Directory.SetCurrentDirectory(AppContext.BaseDirectory);
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    ContentRootPath = WindowsServiceHelpers.IsWindowsService() ? AppContext.BaseDirectory : default
+});
+
+// Path
+builder.Configuration.SetBasePath(AppContext.BaseDirectory);
+
+// Service
+builder.Host
+    .UseWindowsService()
+    .UseSystemd();
+
+// Logging
+builder.Logging.ClearProviders();
+builder.Services.AddSerilog(options => options.ReadFrom.Configuration(builder.Configuration));
+
+// Add services to the container.
+
+//--------------------------------------------------------------------------------
+// Configure the HTTP request pipeline.
+//--------------------------------------------------------------------------------
+var app = builder.Build();
+
+app.Run();
